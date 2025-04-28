19cea81c62d86a6405685c572ba694bab021f70f
Fabien Amann <fabien@feub.net>
2025-04-28 10:43:54 +0200

    feat: added Fancybox (project images)

--- src/components/cv/content/projects.tsx
+++ src/components/cv/content/projects.tsx
@@ -1,9 +1,13 @@
-import type { ReactNode } from "react";
+import { useEffect, type ReactNode } from "react";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
 import { CodeBracketSquareIcon } from "@heroicons/react/24/outline";
 
 type ProjectItem = {
+  id: number;
   name: string;
   description: string;
   technos: string;
@@ -14,6 +18,7 @@ type ProjectItem = {
 
 const projects_recent: ProjectItem[] = [
   {
+    id: 1,
     name: "ClubCompta - Application de Gestion de Budget Associatif",
     description:
       "Ce projet, sur lequel j'ai eu le plaisir de collaborer avec une équipe talentueuse, a été réalisé dans le cadre de ma formation en Conception et Développement d’Applications à la Wild Code School. Notre objectif était de créer une interface intuitive répondant aux besoins spécifiques des associations, afin de faciliter leur gestion financière.",
@@ -25,6 +30,7 @@ const projects_recent: ProjectItem[] = [
       "https://github.com/WildCodeSchool-CDA-FT-2024-09/JS-CDA-Projet-2-Team-D",
   },
   {
+    id: 2,
     name: "LibTrack (API + client)",
     description:
       "Projet personnel d'une API (Symfony) de gestion de collection musicale sur supports physiques (CD, vinyles, etc.) avec un client React.",
@@ -38,6 +44,7 @@ const projects_recent: ProjectItem[] = [
     githubLink: "https://github.com/feub/libtrack-sy",
   },
   {
+    id: 3,
     name: "LibTrack Scanner (client mobile)",
     description:
       "Client mobile pour l'API LibTrack (ci-dessus) qui permet notamment de scanner les codes barre des supports physiques pour une recherche (API Discogs) et un ajout rapide.",
@@ -60,6 +67,7 @@ const projects_recent: ProjectItem[] = [
   //     githubLink: 'https://github.com/feub/in-my-pantry'
   // },
   {
+    id: 4,
     name: "Feub CV",
     description:
       "Il s'agit d'un site statique type CV en Vue.js + Tailwind, facilement personnalisable, avec mode clair/sombre 🌞 🌚",
@@ -77,6 +85,7 @@ const projects_recent: ProjectItem[] = [
   //     githubLink: 'https://github.com/feub/weather-react'
   // },
   {
+    id: 5,
     name: "Les Douces Nuits de Maé - E-commerce",
     description:
       "Gestion et développements d'outils internes pour 2 sites E-commerce sous Magento, puis Prestashop.",
@@ -86,6 +95,7 @@ const projects_recent: ProjectItem[] = [
     githubLink: "https://www.lesdoucesnuitsdemae.com/",
   },
   {
+    id: 6,
     name: "Expat.com",
     description:
       "Conception et développement du module Événements. Développements PHP divers. Une expérience fascinante pour un site leader mondial dans son domaine, avec plusieurs millions d'utilisateurs et un fort trafic.",
@@ -97,82 +107,74 @@ const projects_recent: ProjectItem[] = [
 ];
 
 function ProjectItem({ project }: { project: ProjectItem }) {
-  return (
-    <>
-      <li className={styles.projectItemContainer}>
-        <img
-          src={useBaseUrl(`/img/${project.logoUrl}`)}
-          alt={project.name}
-          className={styles.projectLogo}
-        />
+  useEffect(() => {
+    Fancybox.bind("[data-fancybox]", {
+      // Options de configuration ici
+    });
+  }, []);
 
-        <div>
-          <h4>{project.name}</h4>
-          <p>
-            {project.technos.split(",").map((tech, idx) => (
-              <span key={idx} className={styles.projectLabel}>
-                {tech}
-              </span>
-            ))}
-          </p>
-          <div>{project.description}</div>
-          <div className={styles.projectItemImagesContainer}>
-            {Array.isArray(project.images)
-              ? project.images.map((image, idx) => (
-                  <a
-                    key={idx}
-                    href={useBaseUrl(`/img/${image}`)}
-                    data-lightbox={`project-${project.name.replace(
-                      /\s+/g,
-                      "-",
-                    )}`}
-                    data-title={`${project.name} screenshot ${idx + 1}`}
-                    onClick={(e) => {
-                      e.preventDefault(); // Prevent default browser navigation
-                      return false;
-                    }}
-                  >
-                    <img
-                      src={useBaseUrl(`/img/${image}`)}
-                      alt={`${project.name} screenshot ${idx + 1}`}
-                      className={styles.projectItemImage}
-                    />
-                  </a>
-                ))
-              : project.images && (
-                  <a
-                    href={useBaseUrl(`/img/${project.images}`)}
-                    data-lightbox={`project-${project.name.replace(
-                      /\s+/g,
-                      "-",
-                    )}`}
-                    data-title={`${project.name} screenshot`}
-                    onClick={(e) => {
-                      e.preventDefault(); // Prevent default browser navigation
-                      return false;
-                    }}
-                  >
-                    <img
-                      src={useBaseUrl(`/img/${project.images}`)}
-                      alt={`${project.name} screenshot`}
-                      className={styles.projectItemImage}
-                    />
-                  </a>
-                )}
-          </div>
+  return (
+    <BrowserOnly fallback={<div>Loading...</div>}>
+      {() => (
+        <li className={styles.projectItemContainer}>
+          <img
+            src={useBaseUrl(`/img/${project.logoUrl}`)}
+            alt={project.name}
+            className={styles.projectLogo}
+          />
+
           <div>
-            <a
-              href={`${project.githubLink}`}
-              target="_blank"
-              className={styles.projectLinkContainer}
-            >
-              <CodeBracketSquareIcon className={styles.projectLinkIcon} />
-              <div>Lien GitHub du projet</div>
-            </a>
+            <h4>{project.name}</h4>
+            <p>
+              {project.technos.split(",").map((tech, idx) => (
+                <span key={idx} className={styles.projectLabel}>
+                  {tech}
+                </span>
+              ))}
+            </p>
+            <div>{project.description}</div>
+            <div className={styles.projectItemImagesContainer}>
+              {Array.isArray(project.images)
+                ? project.images.map((image, idx) => (
+                    <a
+                      key={idx}
+                      href={useBaseUrl(`/img/${image}`)}
+                      data-fancybox={`gallery-${project.id}`}
+                    >
+                      <img
+                        src={useBaseUrl(`/img/${image}`)}
+                        alt={`${project.name} screenshot ${idx + 1}`}
+                        className={styles.projectItemImage}
+                      />
+                    </a>
+                  ))
+                : project.images && (
+                    <a
+                      href={useBaseUrl(`/img/${project.images}`)}
+                      data-fancybox={`gallery-${project.id}`}
+                    >
+                      <img
+                        src={useBaseUrl(`/img/${project.images}`)}
+                        alt={`${project.name} screenshot`}
+                        className={styles.projectItemImage}
+                      />
+                    </a>
+                  )}
+            </div>
+            <div>
+              <a
+                href={`${project.githubLink}`}
+                target="_blank"
+                className={styles.projectLinkContainer}
+              >
+                <CodeBracketSquareIcon className={styles.projectLinkIcon} />
+                <div>Lien GitHub du projet</div>
+              </a>
+            </div>
           </div>
-        </div>
-      </li>
-    </>
+        </li>
+      )}
+    </BrowserOnly>
   );
 }
 

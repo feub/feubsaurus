e8754faffcfc25d03a49f0d7417d237ddb2036cb
Fabien Amann <fabien@feub.net>
2025-05-12 15:09:00 +0200

    chore: updated projects

--- src/components/cv/content/projects.tsx
+++ src/components/cv/content/projects.tsx
@@ -12,8 +12,9 @@ type ProjectItem = {
   description: string;
   technos: string;
   logoUrl: string;
-  images: string | Array<string>;
-  githubLink: string;
+  images?: string | Array<string>;
+  githubLink?: string;
+  siteLink?: string;
 };
 
 const projects_recent: ProjectItem[] = [
@@ -21,20 +22,19 @@ const projects_recent: ProjectItem[] = [
     id: 1,
     name: "ClubCompta - Application de Gestion de Budget Associatif",
     description:
-      "Ce projet, sur lequel j'ai eu le plaisir de collaborer avec une équipe talentueuse, a été réalisé dans le cadre de ma formation en Conception et Développement d’Applications à la Wild Code School. Notre objectif était de créer une interface intuitive répondant aux besoins spécifiques des associations, afin de faciliter leur gestion financière.",
+      "Ce projet, sur lequel j'ai eu le plaisir de collaborer avec une équipe talentueuse, a été réalisé dans le cadre de ma formation en Conception et Développement d’Applications à la Wild Code School. Il s'agit d'une architecture en microservices majoritairement en TypeScript (avec un service en PHP). Notre objectif était de créer une interface intuitive répondant aux besoins spécifiques des associations, afin de faciliter leur gestion financière.",
     technos:
-      "microservices, Docker, TypeScript, Apollo Server/GraphQL, Express.js, React, Slim Framework (PHP), Nodemailer, Redis",
+      "microservices, Docker, TypeScript, Apollo Server/GraphQL, Express.js, React, Slim Framework (PHP), Nodemailer, PostgreSQL, Redis",
     logoUrl: "logo-clubcompta.svg",
-    images: ["clubcompta.jpg", "clubcompta-charts.png", "clubcompta-users.png"],
-    githubLink:
-      "https://github.com/WildCodeSchool-CDA-FT-2024-09/JS-CDA-Projet-2-Team-D",
+    images: ["clubcompta-charts.png", "clubcompta.jpg", "clubcompta-users.png"],
+    githubLink: "https://github.com/feub/ClubCompta",
   },
   {
     id: 2,
     name: "LibTrack (API + client)",
     description:
       "Projet personnel d'une API (Symfony) de gestion de collection musicale sur supports physiques (CD, vinyles, etc.) avec un client React.",
-    technos: "Symfony 7, MySQL, API, React, Shadcn",
+    technos: "Symfony 7, MySQL, API, React, Shadcn, Discogs API",
     logoUrl: "libtrack-native-logo.png",
     images: [
       "libtrack-react-login.png",
@@ -48,7 +48,7 @@ const projects_recent: ProjectItem[] = [
     name: "LibTrack Scanner (client mobile)",
     description:
       "Client mobile pour l'API LibTrack (ci-dessus) qui permet notamment de scanner les codes barre des supports physiques pour une recherche (API Discogs) et un ajout rapide.",
-    technos: "React Native (with Expo)",
+    technos: "React Native",
     logoUrl: "libtrack-native-logo.png",
     images: [
       "libtrack-native-releases.jpg",
@@ -76,33 +76,35 @@ const projects_recent: ProjectItem[] = [
     images: "feub-cv.png",
     githubLink: "https://github.com/feub/feub_cv",
   },
-  // {
-  //     name: 'Weather React',
-  //     description: "Un petit site de météo responsive en React.",
-  //     technos: 'React, Tailwind, OW',
-  //     logoUrl: 'fabien_amann_li.jpg',
-  //     images: 'feub-cv.png',
-  //     githubLink: 'https://github.com/feub/weather-react'
-  // },
   {
-    id: 5,
+    id: 50,
+    name: "Weather React",
+    description:
+      "Une simple application météo utilisant React + Tailwind CSS. Rien de compliqué ici, j'ai développé cette application pour apprendre et pratiquer React.",
+    technos: "React, Tailwind, OpenWeatherMap API, MapBox API",
+    logoUrl: "weather-react.png",
+    images: ["weather-react-cities.png", "weather-react-city.png"],
+    githubLink: "https://github.com/feub/weather-react",
+  },
+  {
+    id: 510,
     name: "Les Douces Nuits de Maé - E-commerce",
     description:
-      "Gestion et développements d'outils internes pour 2 sites E-commerce sous Magento, puis Prestashop.",
+      "Gestion de 2 sites E-commerce sous Magento, puis Prestashop et développements d'outils d'améliorations des ventes et de statistiques.",
     technos: "Magento, Prestashop, PHP",
     logoUrl: "logo-ldndm.png",
     images: "ldndm.png",
-    githubLink: "https://www.lesdoucesnuitsdemae.com/",
+    siteLink: "https://www.lesdoucesnuitsdemae.com/",
   },
   {
-    id: 6,
+    id: 600,
     name: "Expat.com",
     description:
       "Conception et développement du module Événements. Développements PHP divers. Une expérience fascinante pour un site leader mondial dans son domaine, avec plusieurs millions d'utilisateurs et un fort trafic.",
     technos: "PHP, MySQL",
     logoUrl: "expat-logo.webp",
     images: "expat.png",
-    githubLink: "https://www.expat.com/",
+    siteLink: "https://www.expat.com/",
   },
 ];
 
@@ -162,14 +164,26 @@ function ProjectItem({ project }: { project: ProjectItem }) {
                   )}
             </div>
             <div>
-              <a
-                href={`${project.githubLink}`}
-                target="_blank"
-                className={styles.projectLinkContainer}
-              >
-                <CodeBracketSquareIcon className={styles.projectLinkIcon} />
-                <div>Lien GitHub du projet</div>
-              </a>
+              {project.githubLink && (
+                <a
+                  href={`${project.githubLink}`}
+                  target="_blank"
+                  className={styles.projectLinkContainer}
+                >
+                  <CodeBracketSquareIcon className={styles.projectLinkIcon} />
+                  <div>Lien GitHub du projet</div>
+                </a>
+              )}
+              {project.siteLink && (
+                <a
+                  href={`${project.siteLink}`}
+                  target="_blank"
+                  className={styles.projectLinkContainer}
+                >
+                  <CodeBracketSquareIcon className={styles.projectLinkIcon} />
+                  <div>Lien vers le site/projet</div>
+                </a>
+              )}
             </div>
           </div>
         </li>
